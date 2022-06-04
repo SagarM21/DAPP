707f0cc16dece7f2dc4596852e76030941e52213
Sagar Maheshwari <sgr.mhshwr22@gmail.com>
2022-06-04 18:28:06 +0530

    Blockchain connection successful - sending eth

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -1,8 +1,8 @@
-import React from "react";
+import React, { useContext } from "react";
 import { AiFillPlayCircle } from "react-icons/ai";
 import { SiEthereum } from "react-icons/si";
 import { BsInfoCircle } from "react-icons/bs";
-
+import { TransactionContext } from "../context/TransactionContext";
 import { Loader } from ".";
 
 const companyCommonStyles =
@@ -20,9 +20,23 @@ const Input = ({ placeholder, name, type, value, handleChange }) => (
 );
 
 const Welcome = () => {
-	const handleSubmit = (e) => {};
+	const {
+		connectWallet,
+		currentAccount,
+		formData,
+		sendTransaction,
+		handleChange,
+	} = useContext(TransactionContext);
 
-	const connectWallet = () => {};
+	const handleSubmit = (e) => {
+		const { addressTo, amount, keyword, message } = formData;
+
+		e.preventDefault();
+
+		if (!addressTo || !amount || !keyword || !message) return;
+
+		sendTransaction();
+	};
 
 	return (
 		<div className='flex w-full justify-center items-center'>
@@ -35,15 +49,18 @@ const Welcome = () => {
 						Explore the crypto world. Buy and sell cryptocurrencies easily on
 						Krypto.
 					</p>
-
-					<button
-						type='button'
-						onClick={connectWallet}
-						className='flex flex-row justify-center items-center my-5 bg-[#2952e3] p-3 rounded-full cursor-pointer hover:bg-[#2546bd]'
-					>
-						<AiFillPlayCircle className='text-white mr-2' />
-						<p className='text-white text-base font-semibold'>Connect Wallet</p>
-					</button>
+					{!currentAccount && (
+						<button
+							type='button'
+							onClick={connectWallet}
+							className='flex flex-row justify-center items-center my-5 bg-[#2952e3] p-3 rounded-full cursor-pointer hover:bg-[#2546bd]'
+						>
+							<AiFillPlayCircle className='text-white mr-2' />
+							<p className='text-white text-base font-semibold'>
+								Connect Wallet
+							</p>
+						</button>
+					)}
 
 					<div className='grid sm:grid-cols-3 grid-cols-2 w-full mt-10'>
 						<div className={`rounded-tl-2xl ${companyCommonStyles}`}>
@@ -85,25 +102,25 @@ const Welcome = () => {
 							placeholder='Address To'
 							name='addressTo'
 							type='text'
-							handleChange={() => {}}
+							handleChange={handleChange}
 						/>
 						<Input
 							placeholder='Amount (ETH)'
 							name='amount'
 							type='number'
-							handleChange={() => {}}
+							handleChange={handleChange}
 						/>
 						<Input
 							placeholder='Keyword (Gif)'
 							name='keyword'
 							type='text'
-							handleChange={() => {}}
+							handleChange={handleChange}
 						/>
 						<Input
 							placeholder='Enter Message'
 							name='message'
 							type='text'
-							handleChange={() => {}}
+							handleChange={handleChange}
 						/>
 
 						<div className='h-[1px] w-full bg-gray-400 my-2' />
---
+++ client/src/utils/constants.js
@@ -0,0 +1,4 @@
+import abi from "./Transactions.json";
+
+export const contractABI = abi.abi;
+export const contractAddress = "0xEF033CE4417C814E05d8C64E282fe7b61Ed6038F";
